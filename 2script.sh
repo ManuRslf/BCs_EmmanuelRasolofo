2a87693b010eeb2c399be55537b681d3c0f189e6
Rasolofo Emmanuel <rasolof2@login1.bamboo>
2025-04-12 14:17:35 +0200

    bamboo version

--- 2script.sh
+++ 2script.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1                       
 #SBATCH --cpus-per-task=4               
 #SBATCH --gpus=1                   
-#SBATCH --time=2-00:00:00                 
+#SBATCH --time=12:00:00                 
 #SBATCH --partition=public-gpu               
 #SBATCH --mem=32000                      
 
